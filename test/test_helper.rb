b0819df8d8653314cc334eba22cb3a9c85fb1aad
Darren Cauthon <darren@cauthon.com>
2014-01-22 07:06:25 -0600

    Update test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,9 @@
+require 'coveralls'
+Coveralls.wear!
+
 require_relative '../lib/horse'
 require 'minitest/autorun'
 require 'minitest/spec'
 require 'minitest/pride'
 require 'subtle'
 require 'mocha/setup'
-require 'coveralls'
-Coveralls.wear!
